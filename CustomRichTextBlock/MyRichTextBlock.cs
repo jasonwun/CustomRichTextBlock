07ef3afde2199843a967f4b5112986461686eee6
Junda Yin <yinjunda.cool@gmail.com>
2016-01-29 03:18:20 -0500

    Include images and hyperlink feature and binding ability

--- CustomRichTextBlock/MyRichTextBlock.cs
+++ CustomRichTextBlock/MyRichTextBlock.cs
@@ -18,71 +18,75 @@ namespace CustomRichTextBlock
 {
     public sealed class MyRichTextBlock : Control
     {
+        RichTextBlock _richTextBlock;
+        StringBuilder builder = new StringBuilder();
+
+
         public MyRichTextBlock()
         {
             this.DefaultStyleKey = typeof(MyRichTextBlock);
+
             foreach (var key in emojiDict.Keys)
             {
-                builder.Append(key.Replace("[", @"\[").Replace("]", @"\]"));//将字典中的[ 和 ] 符号转换成\]和\[,因为在正则表达式中[
-                                                                            // 和 ] 符号有特殊含义。
+                builder.Append(key.Replace("[", @"\[").Replace("]", @"\]"));
+
                 builder.Append("|");
             }
-            builder.Remove(builder.Length - 1, 1); //去掉最后一个“|”符号，否则匹配时会多出一个""字符。
+            builder.Remove(builder.Length - 1, 1);
         }
 
-        RichTextBlock _richTextBlock;
-        StringBuilder builder = new StringBuilder();
-        Regex urlRx = new Regex(@"(?<url>(http:[/][/]|www.)([a-z]|[A-Z]|[0-9]|[/.]|[~])*)", RegexOptions.IgnoreCase);
-
-        private readonly Dictionary<string, string> emojiDict = new Dictionary<string, string>
-    {
-        {"[哈哈]", "[哈哈]"},
-        {"[嘻嘻]", "[嘻嘻]"},
-        {"[微笑]", "[微笑]"},
-        {"[爱你]", "[爱你]"}
-    };
+        Dictionary<string, string> emojiDict = App.emojiDict;
 
         protected override void OnApplyTemplate()
         {
             _richTextBlock = GetTemplateChild("ChildRichTextBlock") as RichTextBlock;
-            SetRichTextBlock(Text);
         }
 
 
         public String Text
         {
             get { return (String)GetValue(TextProperty); }
-            set
-            {
-                SetValue(TextProperty, value);}
+            set { SetValue(TextProperty, value); }
         }
 
         // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty TextProperty =
-            DependencyProperty.Register(nameof(Text), typeof(String), typeof(MyRichTextBlock), new PropertyMetadata(""));
+            DependencyProperty.Register("Text", typeof(String), typeof(MyRichTextBlock), new PropertyMetadata("", OnTextChange));
+
+        private static void OnTextChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var rtb = (MyRichTextBlock)d;
+            rtb.SetRichTextBlock(e.NewValue.ToString());
+        }
 
         private void SetRichTextBlock(string value)
         {
+            value = value.Replace("<", "&lt;").Replace(">", "&gt;").Replace(" &", "&amp;");
+            if (_richTextBlock == null)
+                return;
+
+            Regex urlRx = new Regex(@"(?<url>(http:[/][/]t.cn[/]\w{7}))", RegexOptions.IgnoreCase);
             MatchCollection matches = urlRx.Matches(value);
-            var r = new Regex(builder.ToString()); 
-            var mc = r.Matches(value); 
-            foreach (Match m in mc) 
+            var r = new Regex(builder.ToString());
+            var mc = r.Matches(value);
+
+
+            foreach (Match m in mc)
             {
-                value = value.Replace(m.Value, string.Format(@"<InlineUIContainer><Border><Image Source=""ms-appx:///Assets/Emoji/{0}.png"" Margin=""2,0,2,0"" Width=""30"" Height=""30""/></Border></InlineUIContainer>", emojiDict[m.Value]));
+                value = value.Replace(m.Value, string.Format(@"<InlineUIContainer><Border><StackPanel Margin = ""2,0,2,0"" Width = ""19"" Height = ""19""><Image Source =""/Assets/Emoji/{0}.png""/></StackPanel></Border></InlineUIContainer> ", emojiDict[m.Value]));
             }
             foreach (Match match in matches)
             {
                 string url = match.Groups["url"].Value;
-                //string newurl = "";
-                //foreach(string i in url)
-                //{
-                //    newurl.ad
-                //}
-                value = value.Replace(url, 
-                    string.Format("<InlineUIContainer><Border><HyperlinkButton Margin=\"0,0,0,-4\" Padding=\"0,2,0,0\" NavigateUri =\"{0}\"><StackPanel HorizontalAlignment=\"Center\" Height=\"25\" Width=\"90\" Background=\"#FFB8E9FF\" Orientation = \"Horizontal\"><Image Margin=\"5,0,0,0\" Source = \"/Assets/Emoji/[哈哈].png\" Width = \"15\" Height = \"15\"/><TextBlock Margin=\"4,2.5,0,0\" Text=\"网页链接\" Foreground=\"White\" FontFamily=\"Microsoft YaHei UI\" FontSize=\"14\" FontWeight=\"Bold\"/></StackPanel ></HyperlinkButton></Border></InlineUIContainer>", url));
+                value = value.Replace(url,
+                    string.Format(@"<InlineUIContainer><Border><HyperlinkButton Margin=""0,0,0,-4"" Padding=""0,2,0,0"" NavigateUri =""{0}""><StackPanel HorizontalAlignment=""Center"" Height=""23"" Width=""87"" Background=""#FFB8E9FF"" Orientation = ""Horizontal"">
+                        <Image Margin=""5,0,0,0"" Source = ""ms-appx:///Assets/Link.png"" Width = ""15"" Height = ""15""/><TextBlock Margin=""4,1.5,0,0"" Text=""网页链接"" Foreground=""White"" FontFamily=""Microsoft YaHei UI"" FontSize=""14"" FontWeight=""Bold""/>
+                    </StackPanel>
+                </HyperlinkButton>
+            </Border>
+        </InlineUIContainer>", url));
             }
-            value = value.Replace("\r\n", "<LineBreak/>"); //将换行符转换成<LineBreak/>,用于实现换行。
-            
+
 
             var xaml = string.Format(@"<Paragraph 
                                         xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
@@ -93,13 +97,9 @@ namespace CustomRichTextBlock
                                     </Paragraph.Inlines>
                                 </Paragraph>", value);
             var p = (Paragraph)XamlReader.Load(xaml);
+            _richTextBlock.Blocks.Clear();
             _richTextBlock.Blocks.Add(p);
+
         }
-
-        
-
-
-
-
     }
 }
